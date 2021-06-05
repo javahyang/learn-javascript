20bb07dafd8d18c15f825e39afc2e954c25ba158
javahyang <javahyang@gmail.com>
2021-06-05 20:37:14 +0900

    중첩배열을 평탄화하는 flat

---
+++ 배열내장함수/flat.js
@@ -0,0 +1,31 @@
+// 다차원 배열을 평탄화하기
+
+const arr1 = [1, 2, [3, 4]];
+arr1.flat();
+// [1, 2, 3, 4]
+
+const arr2 = [1, 2, [3, 4, [5, 6]]];
+arr2.flat();
+// [1, 2, 3, 4, [5, 6]]
+
+const arr3 = [1, 2, [3, 4, [5, 6]]];
+arr3.flat(2);
+// [1, 2, 3, 4, 5, 6]
+
+const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+arr4.flat(Infinity);
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// flat 은 IE 미지원
+// reduce, concat 으로 기능 대체
+
+const arr = [1, 2, [3, 4]];
+
+// To flat single level array
+arr.flat();
+// is equivalent to
+arr.reduce((acc, val) => acc.concat(val), []);
+// [1, 2, 3, 4]
+
+// or with decomposition syntax
+const flattened = arr => [].concat(...arr);
